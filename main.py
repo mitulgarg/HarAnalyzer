774629e7408eec6cade014ca33baf1bbbe28fd2a
Mitul Garg <mitulgarg3@gmail.com>
2025-05-27 15:36:42 +0530

    API Ready for testing on infra

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
+# In-memory database (for demonstration purposes)
+items = []
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+# Pydantic model for item data
+class Item(BaseModel):
+    name: str
+    description: str
+
+# Create an item
+@app.post("/items/", response_model=Item)
+async def create_item(item: Item):
+    item.name=item.name+" after running API"
+    items.append(item)
+    return item
